53062b1a4ab9f12ec1bc68b0fac12536bdc38c9a
rodrigo-marcolino <rodrigomarcolino@gmail.com>
2022-07-03 12:20:07 +1200

    define variables

---
+++ 01-LoadBalance.ps1
@@ -0,0 +1,6 @@
+###########################################
+#Variables for the scope of the project
+$rg = "resourceGroupLoadBalancer"
+$location = "australiaeast"
+$tag = @{"CreatedBy"="t1-candidate"}
+###########################################
